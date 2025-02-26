4fe1c72de81b08b68068fe6470d6c9ef62ded98c
RaulFeier <ionelafeier1@gmail.com>
2025-02-26 08:27:05 +0200

    worked a bit

--- ml.py
+++ ml.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
 # test something
 class Network(object):
     def __init__(self, sizes):
@@ -8,7 +11,9 @@ class Network(object):
         self.biases = [np.random.randn(1, y) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-        print(self.biases)
-        print(self.weights)
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
 
 n = Network([3, 3, 2])
